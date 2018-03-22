922394ecc5edc930f67dba420aba2596fec204d5
WaterTian <changewater@qq.com>
2018-03-22 19:54:45 +0800

    add body  bug have bug

--- js/app.js
+++ js/app.js
@@ -36,9 +36,9 @@ $(document).ready(function() {
 
         var imgContext = imgCanvas.getContext("2d");
 
-        var _scale = (window.innerWidth/_w) *0.8;
+        var _scale = (window.innerWidth / _w) * 0.8;
 
-        imgContext.drawImage(_image, 0, 0, _w, _h, 0, 0, _w*_scale, _h*_scale);
+        imgContext.drawImage(_image, 0, 0, _w, _h, 0, 0, _w * _scale, _h * _scale);
 
 
         var image = imgCanvas.toDataURL("image/png");
@@ -47,14 +47,39 @@ $(document).ready(function() {
         // document.body.removeChild(imgCanvas);
 
         //return;
-        PostImage(imageData, imgContext)
+        // getFace(imageData, imgContext);
+        getBody(imageData, imgCanvas, imgContext);
     }
 
 
-    function PostImage(imageData, imgContext) {
+    function getFace(imageData, imgContext) {
         var apiKey = "DP3p0_hA7ri_cd5sexZryjPdr3Zsrkfd";
         var apiSecret = "1e1isj7MiWSv87sJ3I7iFV8Ow5WgnA6C";
         var url = "https://api-cn.faceplusplus.com/facepp/v3/detect";
+
+        $.post(url, {
+                api_key: apiKey,
+                api_secret: apiSecret,
+                image_base64: imageData,
+                return_attributes: "gender,age,smiling,emotion,ethnicity,beauty,skinstatus"
+            },
+            function(response) {
+                var _json = JSON.parse(response);
+                var _faceArr = _json.faces;
+                var _faceRectangle = _faceArr[0].face_rectangle;
+                addLog(_faceArr[0].attributes);
+
+                imgContext.fillStyle = "rgba(0,0,0,0.5)";
+                imgContext.fillRect(_faceRectangle.left, _faceRectangle.top, _faceRectangle.width, _faceRectangle.height);
+
+            }
+        );
+    };
+
+    function getBody(imageData, imgCanvas, imgContext) {
+        var apiKey = "DP3p0_hA7ri_cd5sexZryjPdr3Zsrkfd";
+        var apiSecret = "1e1isj7MiWSv87sJ3I7iFV8Ow5WgnA6C";
+        var url = "https://api-cn.faceplusplus.com/humanbodypp/beta/segment";
 
         $.post(url, {
                 api_key: apiKey,
@@ -63,17 +88,52 @@ $(document).ready(function() {
             },
             function(response) {
                 var _json = JSON.parse(response);
-                var _faceArr = _json.faces;
-                var _faceRectangle = _faceArr[0].face_rectangle;
+                var grayImg = 'data:image/jpeg;base64,' + _json.result;
                 addLog(_json);
-                addLog(_faceRectangle);
 
-                imgContext.fillStyle = "rgba(0,0,0,0.5)";
-                imgContext.fillRect(_faceRectangle.left, _faceRectangle.top, _faceRectangle.width, _faceRectangle.height);
+
+                var img = document.createElement('img');
+                // img.setAttribute('style', 'display:none;');
+                img.setAttribute('src', grayImg);
+                document.body.appendChild(img);
+
+
+                // imgContext.clearRect(0, 0, imgCanvas.width, imgCanvas.height);
+                // imgContext.drawImage(img, 0, 0);
+
+                // var canvas = document.createElement("canvas");
+                // document.body.appendChild(canvas);
+                // canvas.width = imgCanvas.width;
+                // canvas.height = imgCanvas.height;
+                // var ctx = canvas.getContext("2d");
+                // ctx.drawImage(img, 0, 0);
+
+                // var canvasData = ctx.getImageData(0, 0, imgCanvas.width, imgCanvas.height);
+                // var imgCanvasData = imgContext.getImageData(0, 0, imgCanvas.width, imgCanvas.height);
+
+                // for (var x = 0; x < canvasData.width; x++) {
+                //     for (var y = 0; y < canvasData.height; y++) {
+                //         // Index of the pixel in the array
+                //         var idx = (x + y * canvasData.width) * 4;
+                //         var r = canvasData.data[idx + 0];
+                //         var g = canvasData.data[idx + 1];
+                //         var b = canvasData.data[idx + 2];
+                //         if (r < 30 || g < 30 || b < 30) {
+
+                //             // imgCanvasData.data[idx + 3] = 0; // Alpha channel
+                //         }else{
+                //             // console.log(r);
+                //         }
+                //     }
+                // }
+
+                // imgContext.putImageData(imgCanvasData, 0, 0);
 
             }
         );
     };
+
+
 
 
     function addLog(data) {
